29356196d2f3aa109ba37a0bb67b9e2f00ecd13b
Jasper <japsu.honkasalo@gmail.com>
2024-10-03 10:08:17 +0300

    Code cleanup

--- src/Assets/Code/Scripts/Player/InteractionSystem/WorldObject.cs
+++ src/Assets/Code/Scripts/Player/InteractionSystem/WorldObject.cs
@@ -9,21 +9,15 @@ namespace Player.InteractionSystem
     /// </summary>
     public abstract class WorldObject : InteractableBehaviour
     {
-        /// <summary>
-        /// Allows you to position the object in the world.
-        /// Hold the right mouse button while grabbing to rotate the object around the X and Y axis.
-        /// </summary>
+        [Tooltip("Whether you can position the object in the world.\n" +
+                 "Hold the right mouse button while grabbing to rotate the object around the X and Y axis.")]
         [Header("Interaction Settings")]
         [SerializeField] private bool _canBeGrabbed;
-        
-        /// <summary>
-        /// Places the item in your hand.
-        /// </summary>
+
+        [Tooltip("Whether the item can be placed in your hand.")]
         [SerializeField] private bool _canBeHeld;
-        
-        /// <summary>
-        /// Places the item in your inventory.
-        /// </summary>
+
+        [Tooltip("Whether the item can be placed in your inventory.")]
         [SerializeField] private bool _canBeCollected;
         
         
@@ -36,6 +30,118 @@ namespace Player.InteractionSystem
         private IInteraction[] _supportedInteractions;
         [SerializeField, ReadOnly]
         private string[] _supportedInteractionNamesCache;
+        
+        /// <summary>
+        /// Whether the object supports the "Use" interaction type.<br/>
+        /// If true, the <see cref="UseStart"/> and <see cref="UseStop"/> methods should be overridden.
+        /// </summary>
+        protected abstract bool SupportsUseInteraction { get; }
+
+
+#region Public API
+
+        /// <summary>
+        /// Override to implement a dynamic name.
+        /// </summary>
+        /// <returns>The name of the object, shown in the interaction menu.</returns>
+        public override string GetName() => _name;
+        
+        
+        /// <summary>
+        /// Override to implement a dynamic description.
+        /// </summary>
+        /// <returns>The description of the object, shown in the interaction menu.</returns>
+        public override string GetDescription() => _description;
+
+
+        public void GrabStart() => Debug.LogError("Grabbing not implemented.");
+        public void GrabStop() => Debug.LogError("Grabbing not implemented.");
+        public void Hold() => Debug.LogError("Holding not implemented.");
+        public void Collect() => Debug.LogError("Collecting/Inventory not implemented.");
+
+
+        public sealed override void InteractionStart(int index)
+        {
+            if (_supportedInteractions == null)
+            {
+                Debug.LogWarning("FindSupportedInteractions has not been called. Did you accidentally override Awake() without calling base.Awake()?");
+                return;
+            }
+            if (_supportedInteractions.Length == 0)
+                return;
+            
+            if (index < 0 || index >= _supportedInteractions.Length)
+            {
+                Debug.LogWarning($"Invalid interaction index: {index}. Expected 0 to {_supportedInteractions.Length - 1}.");
+                return;
+            }
+            
+            if (_currentInteraction != null)
+            {
+                Debug.LogWarning("Interaction already in progress. Stopping current interaction.");
+                _currentInteraction.Stop();
+            }
+            
+            _currentInteraction = _supportedInteractions[index];
+            _currentInteraction!.Start();
+        }
+
+
+        public sealed override void InteractionStop()
+        {
+            if (_supportedInteractions == null)
+            {
+                Debug.LogWarning("FindSupportedInteractions has not been called. Did you accidentally override Awake() without calling base.Awake()?");
+                return;
+            }
+            if (_supportedInteractions.Length == 0)
+                return;
+
+            if (_currentInteraction == null)
+            {
+                Debug.LogWarning("No interaction to stop.");
+                return;
+            }
+            
+            _currentInteraction.Stop();
+            _currentInteraction = null;
+        }
+        
+        
+        public sealed override string[] GetSupportedInteractionNames() => _supportedInteractionNamesCache;
+
+#endregion
+
+
+#region Protected API
+
+        /// <summary>
+        /// Override to add custom interactions to the object.
+        /// </summary>
+        /// <param name="supportedInteractions">The list to add interactions to.</param>
+        protected virtual void AddCustomInteractions(List<IInteraction> supportedInteractions) { }
+        
+        
+        /// <summary>
+        /// Override to implement the "Use" interaction type.
+        /// Called when the player presses the "interact" key.
+        /// 
+        /// <remarks>
+        /// Only called if <see cref="SupportsUseInteraction"/> is true.
+        /// </remarks>
+        /// </summary>
+        protected virtual void UseStart() { }
+        
+        
+        /// <summary>
+        /// Override to implement the "Use" interaction type.
+        /// Called when the player releases the "interact" key.
+        /// 
+        /// <remarks>
+        /// Only called if <see cref="SupportsUseInteraction"/> is true.
+        /// </remarks>
+        /// </summary>
+        protected virtual void UseStop() { }
 
 
         protected virtual void Awake()
@@ -43,6 +149,10 @@ namespace Player.InteractionSystem
             FindSupportedInteractions();
         }
 
+#endregion
+
+
+#region Private API
 
         private void FindSupportedInteractions()
         {
@@ -74,72 +184,6 @@ namespace Player.InteractionSystem
             if (SupportsUseInteraction) supportedInteractions.Add(new ActionInteraction("Use", UseStart, UseStop));
         }
 
-
-        protected virtual void AddCustomInteractions(List<IInteraction> supportedInteractions)
-        {
-            // Override this method to add custom interactions.
-        }
-
-
-        public void GrabStart() => throw new System.NotImplementedException();
-        public void GrabStop() => throw new System.NotImplementedException();
-        public void Hold() => throw new System.NotImplementedException();
-        public void Collect() => throw new System.NotImplementedException();
-        protected virtual void UseStart() { }
-        protected virtual void UseStop() { }
-        protected abstract bool SupportsUseInteraction { get; }
-
-
-        public override string GetName() => _name;
-        public override string GetDescription() => _description;
-        public sealed override string[] GetSupportedInteractionNames() => _supportedInteractionNamesCache;
-
-
-        public override void InteractionStart(int index)
-        {
-            if (_supportedInteractions == null)
-            {
-                Debug.LogWarning("FindSupportedInteractions has not been called. Did you accidentally override Awake() without calling base.Awake()?");
-                return;
-            }
-            if (_supportedInteractions.Length == 0)
-                return;
-            
-            if (index < 0 || index >= _supportedInteractions.Length)
-            {
-                Debug.LogWarning($"Invalid interaction index: {index}. Expected 0 to {_supportedInteractions.Length - 1}.");
-                return;
-            }
-            
-            if (_currentInteraction != null)
-            {
-                Debug.LogWarning("Interaction already in progress. Stopping current interaction.");
-                _currentInteraction.Stop();
-            }
-            
-            _currentInteraction = _supportedInteractions[index];
-            _currentInteraction!.Start();
-        }
-
-
-        public override void InteractionStop()
-        {
-            if (_supportedInteractions == null)
-            {
-                Debug.LogWarning("FindSupportedInteractions has not been called. Did you accidentally override Awake() without calling base.Awake()?");
-                return;
-            }
-            if (_supportedInteractions.Length == 0)
-                return;
-
-            if (_currentInteraction == null)
-            {
-                Debug.LogWarning("No interaction to stop.");
-                return;
-            }
-            
-            _currentInteraction.Stop();
-            _currentInteraction = null;
-        }
+#endregion
     }
 }
